488a7e52bdf7d92e5f2941fe4cc743e37c9e8984
vinodkumaryadlapalli <115680367+vinodkumaryadlapalli@users.noreply.github.com>
2022-10-13 10:59:13 -0400

    updated

--- SampleTest/Services/BookService.cs
+++ SampleTest/Services/BookService.cs
@@ -11,9 +11,10 @@ namespace Test_2022F.Services
         public bool IsPublicDomain(Book book)
         {
             // TestNote: Return true if the book was published 75 years ago or more
-            int book1 = Convert.ToInt32(book.Published);
-
-            return book1 > 75;
+             int book1 = Convert.ToInt32(book.Published.Year);
+            DateTime currentDate = new DateTime(2022-10-13);
+            int CalculatedDate = Convert.ToInt32( currentDate.Year - book1);
+            return CalculatedDate > 75;
         }
     }
 }
